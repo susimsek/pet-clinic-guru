a4ec743554cf707f91b0a33f694d77bc4e38d7b7
Şuayb Şimşek <suayb.simsek@detaysoft.com>
2020-08-24 20:22:33 +0300

    Initial Project Add

---
+++ src/main/java/com/springframework/petclinicguru/PetClinicGuruApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.petclinicguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicGuruApplication.class, args);
+    }
+
+}
